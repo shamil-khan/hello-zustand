e3be1dc1bbd43aa4a2bc9e8b32e5d9bc31382a59
Shamil Khan <shamil.ahmed@gmail.com>
2026-01-07 02:46:08 +0500

    [pkg] added prettier and config and applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import '@/App.css'
+import '@/App.css';
 
 export function App() {
   return (
     <>
       <h1>Hello Vite + React</h1>
-      <p className="read-the-docs text-red-600">
-        This is test
-      </p>
+      <p className='read-the-docs text-red-600'>This is test</p>
     </>
-  )
+  );
 }
